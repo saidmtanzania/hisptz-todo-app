9a24f79a7f96acd6ec2bd7637bfc9536eafd3038
saidmtanzania <saidmtanzania@gmail.com>
2023-05-31 21:07:31 +0300

    refactor: setup full development environment with eslint prettier and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Below</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
